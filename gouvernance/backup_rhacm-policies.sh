a1a5d414e7bd536b46f00d1d013d1c8ca9a9395e
Sebastien Chabrolles <s.chabrolles@fr.ibm.com>
2024-02-21 11:57:56 +0100

    First Commit

---
+++ gouvernance/backup_rhacm-policies.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+PROJECT=rhacm-policies
+
+for type in policies policysets; do
+  mkdir -p $type
+  cd $type
+  oc get $type -n $PROJECT -o yaml | yq '.items[] | split_doc | del(.status,.metadata.managedFields,.metadata.resourceVersion,.metadata.uid,.metadata.generation,.metadata.creationTimestamp)' | yq --no-doc -s '.metadata.name +".yaml"'
+  cd ..
+done
+
+
+mkdir -p secrets
+cd secrets
+oc get secrets -n $PROJECT -o yaml | yq '.items[] | select(.type=="Opaque") | split_doc | del(.status,.metadata.managedFields,.metadata.resourceVersion,.metadata.uid,.metadata.generation,.metadata.creationTimestamp)' | yq --no-doc -s '.metadata.name +".yaml"'
+cd ..
